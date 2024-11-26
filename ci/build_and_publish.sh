0351f02a73e9a5e22031a9a92d06370903279912
xosnrdev <successxodev@gmail.com>
2024-11-26 09:41:53 -0500

    feat(ci): Add script to build and publish cargonode release for aarch64-apple-darwin target

---
+++ ci/build_and_publish.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# This script builds a cargonode release for the aarch64-apple-darwin target.
+# At time of writing, GitHub Actions does not free Apple silicon
+# runners. Since I have somewhat recently acquired an M2 mac mini, I just use
+# this script to build the release tarball and upload it with `gh`.
+#
+# Once GitHub Actions has proper support for Apple silicon, we should add it
+# to our release workflow and drop this script.
+
+set -e
+
+version="$1"
+if [ -z "$version" ]; then
+    echo "missing version" >&2
+    echo "Usage: "$(basename "$0")" <version>" >&2
+    exit 1
+fi
+if ! grep -q "version = \"$version\"" Cargo.toml; then
+    echo "version does not match Cargo.toml" >&2
+    exit 1
+fi
+
+target=aarch64-apple-darwin
+cargo build --release --features pcre2 --target $target
+BIN=target/$target/release/cargonode
+NAME=cargonode-$version-$target
+ARCHIVE="deployment/m2/$NAME"
+
+mkdir -p "$ARCHIVE"/{complete,doc}
+cp "$BIN" "$ARCHIVE"/
+strip "$ARCHIVE/cargonode"
+cp {README.md,LICENSE-MIT,LICENSE-APACHE} "$ARCHIVE"/
+cp {CHANGELOG.md} "$ARCHIVE"/doc/
+
+tar c -C deployment/m2 -z -f "$ARCHIVE.tar.gz" "$NAME"
+shasum -a 256 "$ARCHIVE.tar.gz" >"$ARCHIVE.tar.gz.sha256"
+gh release upload "$version" "$ARCHIVE.tar.gz" "$ARCHIVE.tar.gz.sha256"
