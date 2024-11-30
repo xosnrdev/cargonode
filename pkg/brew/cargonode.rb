62c413b54cba2d89225a9a2c557bebeace2ff4e7
xosnrdev <successxodev@gmail.com>
2024-11-30 16:11:12 -0500

    refactor(Homebrew): update CLI tool description and features

--- pkg/brew/cargonode.rb
+++ pkg/brew/cargonode.rb
@@ -1,5 +1,5 @@
 class Cargonode < Formula
-    desc "Streamline Node.js development workflows"
+    desc "CLI for Node.js development workflows"
     homepage "https://github.com/xosnrdev/cargonode"
     url "https://github.com/xosnrdev/cargonode/archive/refs/tags/0.1.0.tar.gz"
     sha256 "42a936c2e863ebca38319c976dd229c38d0b5040862b14c6cf450b54ec7e5276"
@@ -10,19 +10,21 @@ class Cargonode < Formula
       url :stable
       strategy :github_latest
     end
+
+    depends_on "rust" => :build
   
     bottle do
-        sha256 cellar: :any,                 arm64_sonoma:   "f2e7750b013fc8a01d759edc1a8e200721471d241fafcf045d71c36b9129cad2"
-        sha256 cellar: :any,                 arm64_ventura:  "f2e7750b013fc8a01d759edc1a8e200721471d241fafcf045d71c36b9129cad2"
-        sha256 cellar: :any,                 arm64_monterey: "f2e7750b013fc8a01d759edc1a8e200721471d241fafcf045d71c36b9129cad2"
-        sha256 cellar: :any,                 sonoma:         "879d120c3b15cd1cd87fa0b3f4dc4fa54ad6c632712d9a8646be7697863fbb45"
-        sha256 cellar: :any,                 ventura:        "879d120c3b15cd1cd87fa0b3f4dc4fa54ad6c632712d9a8646be7697863fbb45"
-        sha256 cellar: :any,                 monterey:       "879d120c3b15cd1cd87fa0b3f4dc4fa54ad6c632712d9a8646be7697863fbb45"
-        sha256 cellar: :any_skip_relocation, x86_64_linux:   "eda2b34b131071a7ae5e8e30df13b7883a63d06038a26ecbb729c376d7df34fe"
+      sha256 cellar: :any,                 arm64_sonoma:   "ee67350dad5a14358be0dab0c494e6e7023ee2aa8a77d43457997650a8afc837"
+      sha256 cellar: :any,                 arm64_ventura:  "ee67350dad5a14358be0dab0c494e6e7023ee2aa8a77d43457997650a8afc837"
+      sha256 cellar: :any,                 arm64_monterey: "ee67350dad5a14358be0dab0c494e6e7023ee2aa8a77d43457997650a8afc837"
+      sha256 cellar: :any,                 sonoma:         "e57e896300dcb33b4d64a99a227fdb288625dde11adaa73f91b9fbd866ee2be4"
+      sha256 cellar: :any,                 ventura:        "e57e896300dcb33b4d64a99a227fdb288625dde11adaa73f91b9fbd866ee2be4"
+      sha256 cellar: :any,                 monterey:       "e57e896300dcb33b4d64a99a227fdb288625dde11adaa73f91b9fbd866ee2be4"
+      sha256 cellar: :any_skip_relocation, x86_64_linux:   "310995033f19ab7520815afe3cf847b1884a4d58f19c34e37c519d23ee759cb3"
     end
   
     def install
-      bin.install "cargonode"
+      system "cargo", "install", *std_cargo_args
     end
   
     test do
