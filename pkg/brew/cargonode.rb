e99ec3ac1200717c83095f6c117a0f6bcc92c929
xosnrdev <successxodev@gmail.com>
2024-11-30 08:45:35 -0500

    chore(homebrew): symbolic link

--- pkg/brew/cargonode.rb
+++ pkg/brew/cargonode.rb
@@ -2,6 +2,7 @@ class Cargonode < Formula
     version '0.1.0'
     desc "Streamline Node.js development workflows"
     homepage "https://github.com/xosnrdev/cargonode"
+    license "MIT OR Apache-2.0"
   
     if OS.mac?
         url "https://github.com/xosnrdev/cargonode/releases/download/#{version}/cargonode-#{version}-x86_64-apple-darwin.tar.gz"
@@ -13,6 +14,10 @@ class Cargonode < Formula
   
     def install
       bin.install "cargonode"
+    end
+
+    test do
+        system "#{bin}/cargonode", "--version"
     end
 
 end
