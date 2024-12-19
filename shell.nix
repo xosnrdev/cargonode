968319250b69b0044e35e5b04c3d62538ff389a1
xosnrdev <successxodev@gmail.com>
2024-12-19 11:19:36 -0500

    chore(nixfmt): drop the shell script for nixfmt-rfc-style

--- shell.nix
+++ shell.nix
@@ -1,8 +1,13 @@
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 pkgs.mkShell {
   # Get dependencies from the main package
   inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
   # Additional tooling
-  buildInputs = with pkgs; [ bzip2 ];
+  buildInputs = with pkgs; [
+    bzip2
+    nixfmt-rfc-style
+  ];
   nativeBuildInputs = with pkgs; [ pkg-config ];
 }
