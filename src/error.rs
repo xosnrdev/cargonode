07af321cd476f5aae223302692675e4600ac124a
Success Kingsley <successxodev@gmail.com>
2025-01-11 00:41:42 -0500

    feat: Add centralized error module with `anyhow` crate (#27)
    - Introduced `anyhow` crate for improved user-facing error.
    - Created `error.rs` module containing the `CliError` struct and associated methods for error handling.
    - Added error module to lib.

---
+++ src/error.rs
@@ -0,0 +1,60 @@
+#[derive(Debug)]
+pub struct CliError {
+    error: Option<anyhow::Error>,
+    code: i32,
+}
+
+impl CliError {
+    pub fn silent_with_code(code: i32) -> Self {
+        Self { error: None, code }
+    }
+
+    pub fn silent() -> Self {
+        Self::silent_with_code(0)
+    }
+
+    pub fn message_with_code(e: impl Into<anyhow::Error>, code: i32) -> Self {
+        Self {
+            error: Some(e.into()),
+            code,
+        }
+    }
+
+    pub fn message(e: impl Into<anyhow::Error>) -> Self {
+        Self::message_with_code(e, 101)
+    }
+}
+
+macro_rules! process_error_from {
+    ($from:ty) => {
+        impl From<$from> for CliError {
+            fn from(error: $from) -> Self {
+                Self::message(error)
+            }
+        }
+    };
+}
+
+process_error_from!(anyhow::Error);
+process_error_from!(std::io::Error);
+process_error_from!(std::string::FromUtf8Error);
+
+impl From<i32> for CliError {
+    fn from(code: i32) -> Self {
+        Self::silent_with_code(code)
+    }
+}
+
+impl std::fmt::Display for CliError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(error) = self.error.as_ref() {
+            error.fmt(f)
+        } else {
+            write!(f, "Exit code: {}", self.code)
+        }
+    }
+}
+
+impl std::error::Error for CliError {}
+
+pub type AppResult<T> = anyhow::Result<T>;
